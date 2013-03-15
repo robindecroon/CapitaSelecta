d1e54379c6e22145836e66f18c6ba27628523be0
Stijn <stijn.adams@student.kuleuven.be>
2013-03-15 15:40:39 +0100

    Database: 	- initieel 	- papers

---
+++ src/database/SQLAuthor.java
@@ -0,0 +1,36 @@
+package database;
+
+public class SQLAuthor {
+
+	private String first;
+	private String last;
+	private String country;
+	private int univ;
+	
+	public String getFirst() {
+		return first;
+	}
+	public void setFirst(String first) {
+		this.first = first;
+	}
+	public String getLast() {
+		return last;
+	}
+	public void setLast(String last) {
+		this.last = last;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public int getUniv() {
+		return univ;
+	}
+	public void setUniv(int univ) {
+		this.univ = univ;
+	}
+	
+	
+}
