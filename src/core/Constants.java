d85c91aedd492169fa79848a1cbc74956ccfe978
Robin De Croon <robin.decroon@student.kuleuven.be>
2013-03-15 12:53:19 +0100

    Nieuwe Constants klasse

---
+++ src/core/Constants.java
@@ -0,0 +1,10 @@
+package core;
+
+public class Constants {
+
+	public static final String APP_NAME = "Finding Visos Visualization";
+	
+	public final static String TEXTBOX1 = "Keyword 1";
+	public final static String TEXTBOX2 = "Keyword 2";
+
+}
--- src/core/Main.java
+++ src/core/Main.java
@@ -5,7 +5,6 @@ package core;
 
 import java.awt.BorderLayout;
 import java.awt.Button;
-import java.awt.Checkbox;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Frame;
@@ -15,8 +14,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -24,21 +21,16 @@ import processing.core.PApplet;
 
 /**
  * @author Robin
- *
+ * 
  */
 public class Main {
-	
-	private final static String text1 = "Keyword 1";
-	private final static String text2 = "Keyword 2";
-	
-	private static boolean showCountries;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		final PApplet mainApplet = new MainApplet();
-		Frame frame = new Frame("Geo-Visualizatie Finding Visos");
+		Frame frame = new Frame(Constants.APP_NAME);
 		frame.setLayout(new BorderLayout());
 
 		frame.addWindowListener(new WindowAdapter() {
@@ -50,8 +42,8 @@ public class Main {
 		Panel toolbar = new Panel();
 		toolbar.setLayout(new FlowLayout(FlowLayout.LEFT));
 
-		final TextField tf1 = new TextField(text1, 30);
-		tf1.setBackground(new Color(255,0,0));
+		final TextField tf1 = new TextField(Constants.TEXTBOX1, 30);
+		tf1.setBackground(Color.RED);
 		tf1.addFocusListener(new FocusListener() {
 
 			@Override
@@ -68,8 +60,8 @@ public class Main {
 		});
 		toolbar.add(tf1);
 
-		final TextField tf2 = new TextField(text2, 30);
-		tf2.setBackground(new Color(0,255,0));
+		final TextField tf2 = new TextField(Constants.TEXTBOX2, 30);
+		tf2.setBackground(Color.GREEN);
 		tf2.addFocusListener(new FocusListener() {
 
 			@Override
@@ -87,38 +79,29 @@ public class Main {
 		toolbar.add(tf2);
 		toolbar.setBackground(new Color(240, 240, 240));
 
-		final Checkbox box = new Checkbox("Show Countries");
-		box.addItemListener(new ItemListener() {
-			
-			@Override
-			public void itemStateChanged(ItemEvent arg0) {
-				showCountries = box.getState();
-			}
-		});
-		
-		toolbar.add(box);
-
 		Button searchButton = new Button("Draw");
 		searchButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO
-//				List<Location> searchResults = searchKeyword(model,
-//						tf1.getText());
-//				if(showCountries) {
-//					((MainApplet) unfoldingApplet).initCountries(searchCountry(model, tf1.getText()));					
-//				} else {
-//					((MainApplet) unfoldingApplet).clearCountries();
-//				}
-//				((MainApplet) unfoldingApplet).initLocations(searchResults);
-//				List<Location> searchResults2 = searchKeyword(model,
-//						tf2.getText());
-//				((MainApplet) unfoldingApplet).initLocations2(searchResults2);
+				// List<Location> searchResults = searchKeyword(model,
+				// tf1.getText());
+				// if(showCountries) {
+				// ((MainApplet)
+				// unfoldingApplet).initCountries(searchCountry(model,
+				// tf1.getText()));
+				// } else {
+				// ((MainApplet) unfoldingApplet).clearCountries();
+				// }
+				// ((MainApplet) unfoldingApplet).initLocations(searchResults);
+				// List<Location> searchResults2 = searchKeyword(model,
+				// tf2.getText());
+				// ((MainApplet)
+				// unfoldingApplet).initLocations2(searchResults2);
 			}
 		});
 		toolbar.add(searchButton);
-		
 
 		mainApplet.init();
 		frame.add(toolbar, BorderLayout.NORTH);
